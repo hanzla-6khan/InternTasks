da1ccd5cb9c761b7936383de0f8642b37ebbfede
hanzla-6khan <hanzalakhan101010@gmail.com>
2023-11-29 21:56:54 +0500

    js tasks

---
+++ jspractice/task1.js
@@ -0,0 +1,60 @@
+/*
+//  ============ taks 1 ==========
+
+var petDog = "Rex";
+var petCat = "Petter";
+
+console.log(petDog);
+console.log(petCat);
+console.log("my pet dog's name is " + petDog);
+console.log("my pet cat's name is " + petCat);
+
+var catsound = "purr";
+var dogSound = "woof";
+console.log(petDog + "  says  " + dogSound);
+console.log(petCat + "   says  " + catsound);
+catsound = "meow";
+console.log(petCat + " now says " + catsound);
+
+
+*/
+
+// ============ task 1 Using the logical && operator===========
+// const score = 8;
+// const result = score > 0 && score < 9;
+// console.log("Mid-level-skilles:" + result);
+
+// ==============task2  Using the logical || operator =========
+// const timeremaning = 0;
+// const energy = 10;
+// console.log("Game Over : " + (timeremaning == 0 || energy == 0));
+
+// =============task 3 Using the modulus operator ============
+// var num1 = 2;
+// var num2 = 5;
+// var test1 = num1 % 2;
+// var test2 = num2 % 2;
+
+// const result1 = test1 == 0;
+// const result2 = test2 == 0;
+
+// console.log("IS " + num1 + "an even number " + result1);
+// console.log("IS " + num2 + "an off number" + result2);
+
+// ============task 4 Add numbers using the + operator ===========
+// const a = 5;
+// const b = 10;
+// console.log(a + b);
+
+// =====task 5   Concatenate numbers and strings =========
+/*const now = "Now in";
+const three = 3;
+const d = "D!";
+console.log(now + three + d);*/
+
+// ==========task 6  Use the += operator to accumulate values in a variable ==========
+
+var counter = 0;
+counter += 5;
+counter += 3;
+console.log(counter);
